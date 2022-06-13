7e41e7a075afc1f8a8e573c31c127bb148a5626d
MRizkiirvansyah <kiki.irvansyah@gmail.com>
2022-06-14 01:28:27 +0700

    chore:Foldersturcture

---
+++ frontend/src/pages/User/LandingPage/landingpage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class landingpage extends Component {
+  render() {
+    return (
+      <div>landingpage</div>
+    )
+  }
+}
---
+++ frontend/src/pages/User/PengumumanBeasiswa/pengumumanbeasiswa.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class pengumumanbeasiswa extends Component {
+  render() {
+    return (
+      <div>pengumumanbeasiswa</div>
+    )
+  }
+}
