8f68f60a686a7c9ac96633e65e4ed0cd3cf8844c
Luchiana <frluchiana55@gmail.com>
2022-06-16 11:11:54 +0700

    create tabel

--- backend/db/migration/main.go
+++ backend/db/migration/main.go
@@ -14,56 +14,42 @@ func main() {
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS users (
-    id integer not null primary key AUTOINCREMENT,
-    username varchar(255) not null,
-    password varchar(255) not null,
-    role varchar(255) not null,
-    loggedin boolean not null
-);
+    CREATE TABLE IF NOT EXISTS pengumuman_beasiswa (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nominal NUMERIC NOT NULL,
+        is_diterima VARCHAR(50) NOT NULL,
+        id_user INTEGER NOT NULL,
+        id_beasiswa INTEGER NOT NULL,
+        FOREIGN KEY (id) REFERENCES "user"(id),
+        FOREIGN KEY (id_beasiswa) REFERENCES beasiswa(id_beasiswa)
+    );
 
-CREATE TABLE IF NOT EXISTS products (
-    id integer not null primary key AUTOINCREMENT,
-    product_name varchar(255) not null,
-    category varchar(255) not null,
-    price integer not null,
-    quantity integer not null
-);
+    CREATE TABLE IF NOT EXISTS beasiswa (
+        id_beasiswa INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        jenis_beasiswa TEXT(50) NOT NULL,
+        sumber_beasiswa REAL NOT NULL,
+        tanggal_pendaftaran BLOB NOT NULL,
+        keterangan TEXT(50) NOT NULL
+    );
 
-CREATE TABLE IF NOT EXISTS cart_items (
-    id integer not null primary key AUTOINCREMENT,
-    product_id integer not null,
-    quantity integer not null,
-    FOREIGN KEY (product_id) REFERENCES products(id)
-);
-
-CREATE TABLE IF NOT EXISTS sales (
-    id integer not null primary key AUTOINCREMENT,
-    date DATE not null,
-    product_id integer not null,
-    quantity integer not null,
-    FOREIGN KEY (product_id) REFERENCES products(id)
-);
-
-INSERT INTO users(username, password, role, loggedin) VALUES
-    ('aditira', '1234', 'admin', false),
-    ('dina', '4321', 'employee', false),
-    ('dito', '2552', 'employee', false);
-
-INSERT INTO products(product_name, category, price, quantity) VALUES
-    ('Orange', 'Fruits', 5000, 100),
-    ('Apple', 'Fruits', 2000, 100),
-    ('Melon', 'Fruits', 4000, 100),
-    ('Watermelon', 'Fruits', 10000, 100),
-    ('Banana', 'Fruits', 4000, 100),
-    ('Carrot', 'Vegetables', 2000, 100),
-    ('Broccoli', 'Vegetables', 5200, 100),
-    ('Cucumber', 'Vegetables', 3400, 100),
-    ('Potato', 'Vegetables', 6500, 100),
-    ('Tomato', 'Vegetables', 2200, 100),
-    ('Coffee', 'Drink', 4300, 100),
-    ('Milk', 'Drink', 4000, 100),
-    ('Tea', 'Drink', 2700, 100);`)
+    CREATE TABLE sqlite_sequence(name,seq)}tableuseruser CREATE TABLE "user" (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(50) NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        nisn INTEGER NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        nama VARCHAR(50) NOT NULL,
+        tanggal_lahir BLOB NOT NULL,
+        jenis_kelamin VARCHAR NOT NULL,
+        alamat TEXT(50) NOT NULL,
+        asal_sekolah VARCHAR(50) NOT NULL,
+        pendidikan_terakhir VARCHAR(50) NOT NULL,
+        no_telepon NUMERIC NOT NULL,
+        nama_ortu VARCHAR(50) NOT NULL,
+        pekerjaan_ortu VARCHAR(50) NOT NULL,
+        penghasilan_ortu VARCHAR(50) NOT NULL,
+        "role" VARCHAR NOT NULL);
+    `)
 
 	if err != nil {
 		panic(err)
