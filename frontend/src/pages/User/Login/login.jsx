8f483586c57be2de12b88ab035a341670a0f3133
MRizkiirvansyah <kiki.irvansyah@gmail.com>
2022-06-17 19:01:48 +0700

    chore : fixed responsive

--- frontend/src/pages/User/Login/login.jsx
+++ frontend/src/pages/User/Login/login.jsx
@@ -9,13 +9,24 @@ export default class login extends Component {
         <section id="login">
           <div className="container">
             <div className="row">
-              <div className="col-md-4 my-auto" id="kolom-utama">
+              <div className="col-md-4 my-auto" id="kolom-kiri">
                 <div className="card">
                   <div className="card-body">
                     <form>
                       <div className="masuk text-center">Masuk</div>
                       <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">
+                        <label for="exampleInputNama" className="form-label">
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label for="exampleInputNama" className="form-label">
                           Nama Pengguna
                         </label>
                         <input
@@ -30,7 +41,7 @@ export default class login extends Component {
                           for="exampleInputPassword1"
                           className="form-label"
                         >
-                          Password
+                          Kata Sandi
                         </label>
                         <input
                           type="password"
@@ -42,7 +53,7 @@ export default class login extends Component {
                         type="submit"
                         className="btn btn-primary btn-daftar "
                       >
-                        Daftar
+                        Masuk
                       </button>
                       <div className="teks-bawah">
                         <label className="form-label-bawah">
@@ -56,8 +67,14 @@ export default class login extends Component {
                   </div>
                 </div>
               </div>
-              <div className="col-md-4 d-none d-md-block " id="kolom-kedua">
+              <div className="col-md-4 d-none d-xl-block " id="kolom-kanan">
                 <img src={gambar} className="img-fluid " alt="" />
+              </div>
+              <div className="watermark text-center">
+                <label className="watermark-bottom">SUBAKU @</label>
+                <label className="watermark-bottom">
+                  © 2022 SUBAKU Team.All rights reserved.
+                </label>
               </div>
             </div>
           </div>
