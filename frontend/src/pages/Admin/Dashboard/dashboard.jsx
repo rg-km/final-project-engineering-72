52cba005e88d263e06797bd4142dc535b45d892d
By <endugbandhel@gmail.com>
2022-06-20 15:01:26 +0700

    chore: add dahsboard page

--- frontend/src/pages/Admin/Dashboard/dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard/dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect,useState } from "react";
 import Sidebar from "../../../Components/Sidebar/Sidebar";
-import "./Dashboard.css";
+import "./dashboard.css";
 
 export default function Dashboard() {
     useEffect(() => {
@@ -8,12 +8,21 @@ export default function Dashboard() {
       }, []);
 
     return (
-        <div className="main">
-            <div className="sidebar">
-                <Sidebar/>
-            </div>
-            <div className="container">
-            <img src={require("../../../assets/logo-subaku.png")} alt="logo-app"/>
+        <div className="container-fluid">
+            <div className="row">
+                <nav id="sidebar" className="sidebar">
+                    <Sidebar />
+                </nav>
+                <main className="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item active" aria-current="page">Home</li>
+                        </ol>
+                    </nav>
+                    <div className="main-content">
+                        <img src={require("../../../assets/logo-subaku.png")} alt="logo-app"/>
+                    </div>
+                </main>
             </div>
         </div>
     );
