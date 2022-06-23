fb5ea57b35815fa564d072aa46496a7149ee6cb9
anindyo <anindyo0216@gmail.com>
2022-06-23 16:28:54 +0700

    chore: add DaftarBeasiswa component.

--- frontend/src/pages/User/DaftarBeasiswa/DaftarBeasiswa.jsx
+++ frontend/src/pages/User/DaftarBeasiswa/DaftarBeasiswa.jsx
@@ -1,13 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { useEffect, useState } from "react"
+import FormListBeasiswa from '../../../Components/FormDaftar/FormListBeasiswa'
+import "./DaftarBeasiswa.css"
 
-class daftarbeasiswa extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+export default function DaftarBeasiswa() {
+  const beasiswaList = [
+    {
+      jenisBeasiswa: "Beasiswa Prestasi",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Atlet",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Penelitian",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Prestasi",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Atlet",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Penelitian",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Prestasi",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Atlet",
+    },
+    {
+      jenisBeasiswa: "Beasiswa Penelitian",
+    },
+  ]
+  return (
+    <div className="daftar-bea-wrapper container">
+      <section className="daftar-bea-title">
+        <h1>Daftar Beasiswa</h1>
+      </section>
+      <section className="daftar-bea-list row">
+        {beasiswaList.map((item) => {
+          return (
+            <FormListBeasiswa jenisBeasiswa={item.jenisBeasiswa}/>
+          )
+        })}
+      </section>
+    </div>
+  )
 }
-
-export default daftarbeasiswa;
